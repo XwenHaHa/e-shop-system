342e38e1bea4527c68c424e7df6e85ce501288b5
wen <929716663@qq.com>
2021-08-08 11:41:49 +0800

    完后用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,164 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <span class="big">System</span>
+        <span class="small">@wen</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏的菜单区域 -->
+        <el-menu
+          background-color="#283142"
+          text-color="#fff"
+          active-text-color="#2BB8FA"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+          :default-active="activePath"
+        >
+          <!-- 一级菜单 -->
+          <!-- 在id后面加一个空的字符串
+          实现数值转字符串 -->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menulist"
+            :key="item.id"
+          >
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
+              <!-- 二级菜单的模板区域 -->
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
+  // 数据拿到了为了渲染出来把获取到的数据立即挂载到data中
+  data() {
+    return {
+      // 左侧菜单数据
+      menulist: [],
+      iconObj: {
+        '125': 'iconfont icon-users',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      },
+      // 是否折叠
+      isCollapse: false,
+      // 被激活的链接地址
+      activePath: ''
+    }
+  },
+  created() {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
     },
-  },
+    // 获取所有菜单,发起get请求返回Promise用async和await简化操作，对象解构赋值拿到data
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      // console.log(res)
+    },
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
+    },
+    // 保存链接的激活状态
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #fff;
+  // background-color: #ccc;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 0px;
+  > div {
+    width: 200px;
+    height: 100%;
+    text-align: center;
+    background-color: #2097f0;
+    > .big {
+      font-size: 20px;
+      font-weight: bold;
+      color: #fff;
+      line-height: 60px;
+    }
+    > .small {
+      font-size: 12px;
+      color: #fff;
+    }
+  }
+}
+.el-aside {
+  background-color: #283142;
+  .toggle-button {
+    background-color: #1c2340;
+    color: #fff;
+    font-size: 12px;
+    text-align: center;
+    line-height: 24px;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+  }
+}
+.el-main {
+  background-color: #eff3f6;
+}
+.el-submenu {
+  width: 200px;
+}
+.iconfont {
+  margin-right: 20px;
+}
 </style>
